9bb43c4444e8e2f5e96f8ff7d46ab1c65a1ad383
Guy Marshall <guymarshall9@gmail.com>
2022-05-07 09:47:11 +0100

    Improve algorithm - now calculates number of primes instead of primes up to a number

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import math
 
-def is_prime(number):
+def is_prime_number(number):
     for i in range(2, math.ceil(math.sqrt(number)) + 1):
         if number % i == 0:
             return False
@@ -10,20 +10,17 @@ def main():
     user_input = int(input("Enter a positive integer: "))
     print("Prime numbers:")
 
-    for i in range(user_input + 1):
-        if is_prime(i):
-            print(i)
+    # for i in range(user_input + 1):
+    #     if is_prime(i):
+    #         print(i)
+
+    prime_number_count = 0
+    number = 2
+    while prime_number_count < user_input:
+        is_prime = is_prime_number(number)
+        if is_prime:
+            prime_number_count += 1
+            print(f"{prime_number_count}: {number}")
 
 if __name__ == "__main__":
     main()
-
-# int primeNumberCount = 0;
-# int number = 2;
-# while (primeNumberCount < userInput) {
-#     boolean isPrime = isPrimeNumber(number);
-#     if (isPrime) {
-#         primeNumberCount++;
-#         System.out.printf("%d: %d%n", primeNumberCount, number);
-#     }
-#     number++;
-# }
