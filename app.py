91e45ad569797799f34fd8f537e9388d08df4d4c
Guy Marshall <guy@cslgroupservices.co.uk>
2022-05-11 12:32:01 +0100

    Getting closer to entering prime numbers into database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import math
+import sqlite3
 
 def is_prime_number(number):
     for i in range(2, math.ceil(math.sqrt(number)) + 1):
@@ -14,14 +15,43 @@ def main():
     #     if is_prime(i):
     #         print(i)
 
+    prime_numbers = []
+
     prime_number_count = 0
     number = 2
     while prime_number_count < user_input:
         is_prime = is_prime_number(number)
         if is_prime:
             prime_number_count += 1
-            print(f"{prime_number_count}: {number}")
+            prime_numbers.append({prime_number_count: number})
+            # print(f"{prime_number_count}: {number}")
         number += 1
+
+
+    conn = sqlite3.connect("prime_numbers.db")
+
+    c = conn.cursor()
+
+    c.execute("""CREATE TABLE IF NOT EXISTS prime_numbers (
+        input integer,
+        prime integer
+    )""")
+
+    for prime_number_count,prime_number in enumerate(prime_numbers):
+        c.execute("""INSERT INTO prime_numbers VALUES (
+            :input,
+            :prime
+        )""", {"input": prime_number_count, "prime": prime_number})
+
+    conn.commit()
+
+    c.execute("SELECT * FROM prime_numbers")
+
+    print(c.fetchall())
+
+    conn.commit()
+
+    conn.close()
 
 if __name__ == "__main__":
     main()
