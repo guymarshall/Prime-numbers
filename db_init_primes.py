438248f09f0f17ffac2e3c6bb24913dad2849c02
Guy Marshall <guy@cslgroupservices.co.uk>
2022-05-11 12:12:40 +0100

    Remove unused code

--- db_init_primes.py
+++ db_init_primes.py
@@ -9,31 +9,6 @@ c.execute("""CREATE TABLE IF NOT EXISTS prime_numbers (
     prime integer
 )""")
 
-# c.execute("""INSERT INTO prime_numbers VALUES (
-#     "Corey",
-#     "Schafer",
-#     50000
-# )""")
-
-# emp_1 = Employee("Steve", "Jobs", 100000)
-# emp_2 = Employee("John", "Doe", 24000)
-
-# c.execute("""INSERT INTO prime_numbers VALUES (
-#     "Mary",
-#     "Schafer",
-#     70000
-# )""")
-
-# conn.commit()
-
-# c.execute(f"""INSERT INTO prime_numbers VALUES (
-#     ?,
-#     ?,
-#     ?
-# )""", (emp_1.first, emp_1.last, emp_1.pay))
-
-# conn.commit()
-
 # c.execute(f"""INSERT INTO prime_numbers VALUES (
 #     :first,
 #     :last,
@@ -41,10 +16,6 @@ c.execute("""CREATE TABLE IF NOT EXISTS prime_numbers (
 # )""", {"first": emp_2.first, "last": emp_2.last, "pay": emp_2.pay})
 
 # conn.commit()
-
-# c.execute("SELECT * FROM prime_numbers WHERE last='Schafer'")
-
-# c.execute("SELECT * FROM prime_numbers WHERE last=':last'", {"last": "Doe"})
 
 c.execute("SELECT * FROM prime_numbers")
 
